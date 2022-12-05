d9b9a2d4b1d26c78f52e30494232ec592d9c8ea3
songhaiyu <1740741001@qq.com>
2022-12-05 20:16:07 +0800

    '测试'

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,10 +5,21 @@ module.exports = {
   },
   extends: ["plugin:vue/essential", "eslint:recommended", "plugin:prettier/recommended"],
   parserOptions: {
-    parser: "@babel/eslint-parser"
+    parser: "@babel/eslint-parser",
+    requireConfigFile: false
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+    // "space-before-function-paren": 3 //最少三
+    // "space-before-function-paren": [
+    // 	"error",
+    // 	"never",
+    // 	{
+    // 		anonymous: "always",
+    // 		named: "always",
+    // 		asyncArrow: "always"
+    // 	}
+    // ]
   }
 }
